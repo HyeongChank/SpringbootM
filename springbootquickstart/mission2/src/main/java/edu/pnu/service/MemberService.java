e9312c88f810772f1fd2f1c8ff40eb54925672f5
HyeongChank <jsk381988@gmail.com>
2023-03-26 16:17:20 +0900

    update

--- springbootquickstart/mission2/src/main/java/edu/pnu/service/MemberService.java
+++ springbootquickstart/mission2/src/main/java/edu/pnu/service/MemberService.java
@@ -21,8 +21,8 @@ public class MemberService {
 		return null;
 	}
 	public Member readMember(){
-		memberdao.readMember();
-		return null;
+		
+		return memberdao.readMember();
 	}
 	public Member deleteMember(String pass) {
 		memberdao.deleteMember(pass);
@@ -33,8 +33,8 @@ public class MemberService {
 		return null;
 	}
 	public Member showallMember() {
-		memberdao.showallMember();
 		
-		return null;
+		
+		return memberdao.showallMember();
 	}
 }
