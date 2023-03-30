9327d6e548ca8c30299c1b25d8e002edf5d6b392
HyeongChank <jsk381988@gmail.com>
2023-03-30 18:01:30 +0900

    update_school

--- springbootquickstart/mission4/src/main/java/edu/pnu/dao/logDao.java
+++ springbootquickstart/mission4/src/main/java/edu/pnu/dao/logDao.java
@@ -6,9 +6,6 @@ import edu.pnu.domain.MemberVO;
 
 public interface logDao {
 
-//	Map<String, MemberVO> errMsg(String[] list, Map<String, MemberVO> m);
-//	MemberVO sucMsg(String str, MemberVO mv);
-	
 	Map<String, MemberVO> msg(String[] list, Map<String, MemberVO> m);
 	MemberVO msg(String str, MemberVO mv);
 }
